6b0518a06538b9ba19978ea2058ba527e2365d79
Omar Ashraf <omarashrf15@gmail.com>
2021-10-06 13:33:35 +0200

    Implement DFS in linkparser, returning slice of Link

--- link.go
+++ link.go
@@ -2,13 +2,46 @@ package link
 
 import (
 	"io"
+
+	"golang.org/x/net/html"
 )
 
 type Link struct {
 	Href string
-	Link string
+	Text string
 }
 
 func Parse(r io.Reader) ([]Link, error) {
-	return nil, nil
+	allLinks := []Link{} // make([]Link, 0)
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	var dfs func(*html.Node, bool) string
+	dfs = func(n *html.Node, isParentLink bool) string {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			isParentLink = true
+		}
+		currentText := ""
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			currentText += dfs(c, isParentLink)
+		}
+		if n.Type == html.ElementNode && n.Data == "a" {
+			len := len(n.Attr)
+			for i := 0; i < len; i++ {
+				if n.Attr[i].Key == "href" {
+					var link Link
+					link.Href = n.Attr[i].Val
+					link.Text = currentText
+					allLinks = append(allLinks, link)
+				}
+			}
+		}
+		if n.Type == html.TextNode {
+			currentText += n.Data
+		}
+		return currentText
+	}
+	dfs(doc, false)
+	return allLinks, nil
 }
--- main/main.go
+++ main/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"link-parser"
+	"strings"
+)
 
+var html = `
+<html>
+<body>
+  <a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a>
+</body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(html)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	len := len(links)
+	for i := 0; i < len; i++ {
+		fmt.Printf("Href: %v\nText: %v\n", links[i].Href, links[i].Text)
+	}
 }
