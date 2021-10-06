85c255918e0529cd389f038d0eb70ed0500b7adc
Omar Ashraf <omarashrf15@gmail.com>
2021-10-06 14:27:53 +0200

    cleanup the code

--- link.go
+++ link.go
@@ -12,33 +12,34 @@ type Link struct {
 	Text string
 }
 
+var allLinks []Link // make([]Link, 0)
+
+func dfs(n *html.Node, isParentLink bool) string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		isParentLink = true
+	}
+	currentText := ""
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		currentText += dfs(c, isParentLink)
+	}
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, attr := range n.Attr {
+			if attr.Key == "href" {
+				link := Link{attr.Val, currentText}
+				allLinks = append(allLinks, link)
+				break
+			}
+		}
+	} else if n.Type == html.TextNode {
+		currentText += n.Data
+	}
+	return strings.Join(strings.Fields(currentText), " ") // string.Feilds remove all leading and tailling spaces of the string
+}
+
 func Parse(r io.Reader) ([]Link, error) {
-	allLinks := []Link{} // make([]Link, 0)
 	doc, err := html.Parse(r)
 	if err != nil {
 		return nil, err
-	}
-	var dfs func(*html.Node, bool) string
-	dfs = func(n *html.Node, isParentLink bool) string {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			isParentLink = true
-		}
-		currentText := ""
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			currentText += dfs(c, isParentLink)
-		}
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, attr := range n.Attr {
-				if attr.Key == "href" {
-					link := Link{attr.Val, currentText}
-					allLinks = append(allLinks, link)
-				}
-			}
-		}
-		if n.Type == html.TextNode {
-			currentText += n.Data
-		}
-		return strings.Join(strings.Fields(currentText), " ")
 	}
 	dfs(doc, false)
 	return allLinks, nil
